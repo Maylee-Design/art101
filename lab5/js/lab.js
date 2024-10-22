71ba0cc29e33c8efff985563b702731f670fa6fc
Maylee-Design <niji676@gmail.com>
2024-10-21 20:59:57 -0700

    First time JS

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,3 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -19,3 +12,25 @@ function main() {
 
 // let's get this party started
 main();
+
+//index.js - Learning Javascript
+// Author: Mason Hughes
+//Date: 10.21.24//
+
+// Define Variables
+make = "ford";
+model = "taurus";
+color = "rusty";
+year = 1995;
+
+
+// Calculate
+age = 2024 - year;
+
+
+//Output
+document.writeln("make: " + make + "<br>")
+document.writeln("mode: " + model + "<br>")
+document.writeln("color: " + color + "<br>")
+document.writeln("year: " + year + "<br>")
+document.writeln("age: " + age + "years<br>")
