14ec2548372803a5c3a3c2246fededc94e284e35
Maylee-Design <niji676@gmail.com>
2024-10-23 19:35:21 -0700

    Lab 6 edits

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,40 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+//index.js - Learning Javascript
+// Author: Mason Hughes
+//Date: 10.24.24//
 
-// Constants
+// Variables
+myTransport = ["ebike" , " nissan", " honda"]
 
-// Functions
+//object
+myMainRide = {
+  make: "Nissan",
+  model: "versa",
+  color: "silver",
+  year: "2010",
+  //function in object
+  age: function() {
+      return 2024 - this.year;}
+  }
+
+
+//output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+//write object to document
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
